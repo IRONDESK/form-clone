65a7090006eedff35c79111d4776818192b43352
IRONDESK <zerozerobase@gmail.com>
2022-08-24 21:49:41 +0900

    :tada: 패키지 설치 및 기초 파일 정리

---
+++ src/pages/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">App Start Here</header>
+    </div>
+  );
+}
+
+export default App;
